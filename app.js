a3460e349cbffddcd73663da6a5535de142e5aae
Luis Ivan Jasso Lopez <11995@ulsachihuahua.edu.mx>
2024-10-17 19:36:03 -0600

    Se agregan modelos de usuario y seguimiento

--- app.js
+++ app.js
@@ -3,9 +3,20 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose');
+
+const databaseUrl = "mongodb+srv://user_1:fmGK2fU90Jb0gRnO@practica.m94ua.mongodb.net/cookbook";
+
+mongoose.connect(databaseUrl,{
+  useNewUrlParser: true
+})
+mongoose.connection.on('open',function(){
+  console.log("Connection OK");
+})
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const cookbookRouter = require('./routes/cookbook');
 
 var app = express();
 
@@ -21,6 +32,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/cookbook',cookbookRouter); 
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
