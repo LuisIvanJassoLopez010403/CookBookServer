48d8ffbe450633f5192a3846e44ee493bd19baa3
Luis Ivan Jasso Lopez <11995@ulsachihuahua.edu.mx>
2024-10-24 12:00:36 -0600

    Se configura url para servidor en render

--- app.js
+++ app.js
@@ -4,14 +4,11 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const mongoose = require('mongoose');
-/*const {
+const {
   mongoURL
-} = require('./config').variablesDeConfiguracion;*/
+} = require('./config').variablesDeConfiguracion;
 
-
-const databaseUrl = "mongodb+srv://user_1:fmGK2fU90Jb0gRnO@practica.m94ua.mongodb.net/cookbook";
-
-//const databaseUrl = mongoURL;
+const databaseUrl = mongoURL;
 
 mongoose.connect(databaseUrl,{
   useNewUrlParser: true
