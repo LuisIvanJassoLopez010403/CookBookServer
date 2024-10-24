c51dee91e08c3ff3d781b5a689a03d0d667a2fba
Luis Ivan Jasso Lopez <11995@ulsachihuahua.edu.mx>
2024-10-24 11:50:25 -0600

    Se hacen correcciones al historial

--- controllers/history.controler.js
+++ controllers/history.controler.js
@@ -1,22 +1,49 @@
-const {
-    historyModel
-} = require('../models/history.model');
-const {
-    recipeModel
-} = require('../models/recipe.model');
+const { historyModel } = require('../models/history.model');
 
 async function getHistory(req, res) {
     const idrecipe = req.body.idrecipe;
     const iduser = req.user.id;
 
-    try{
-        const newHistory = new historyModel({
-            idrecipe,
-            iduser
-        });
-        await newHistory.save();
-        res.status(201).json({ message: 'Receta agregada al historial', history: newHistory });
+    try {
+        let userHistory = await historyModel.findOne({ idUsers: iduser });
+        
+        if (!userHistory) {
+            userHistory = new historyModel({
+                idUsers: iduser,
+                idRecipe: [{ recipeId: idrecipe }]  
+            });
+        } else {
+            userHistory.idRecipe.push({
+                recipeId: idrecipe,
+                date: Date.now()
+            });
+        }
+
+        await userHistory.save();
+        res.status(201).json({ message: 'Receta agregada al historial', history: userHistory });
     } catch (error) {
         res.status(500).json({ message: 'Error al agregar la receta', error: error.message });
     }
 };
+async function viewHistory(req, res) {
+    const iduser = req.body.id; 
+
+    try {
+        const history = await historyModel.find({ idUsers: iduser }).populate('idRecipe.recipeId');
+
+        if (!history || history.length === 0) {
+            console.log('User ID:', iduser);
+            return res.status(404).json({ message: 'No se encontró historial para este usuario.' });
+        }
+
+        res.status(200).json(history);
+    } catch (error) {
+        res.status(500).json({ message: 'Error al obtener el historial', error: error.message });
+    }
+}
+
+module.exports = {
+    getHistory,
+    viewHistory
+  }
+  
