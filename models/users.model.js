71bff39a984566ddd8aa42c88bd5cb16fdeee51c
Jimenez <12033@ulsachihuahua.edu.mx>
2024-12-05 21:57:03 -0600

    Se modifica modelo de usuarios

--- models/users.model.js
+++ models/users.model.js
@@ -14,7 +14,7 @@ const usersSchema = new mongoose.Schema({
         required: true
     },
     birthdate: {
-        type: Date,
+        type: String,
         required: true
     },
     gender: {
