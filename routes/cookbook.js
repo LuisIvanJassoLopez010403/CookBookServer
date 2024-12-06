c24bb158e88f51f245b4b5faeb61a372c92f0d71
Jimenez <12033@ulsachihuahua.edu.mx>
2024-12-05 23:55:26 -0600

    Se solucionan errores

--- routes/cookbook.js
+++ routes/cookbook.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
-const {authenticateToken} = require('../middlewares/auth.middleware');
+const {
+    authenticateToken,
+    authorizeRoles
+} = require('../middlewares/auth.middleware');
 
 const {
     signup,
